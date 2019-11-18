988fe15a7670ebc2c43e0b4d6bbd01c5a561be2d
zacharybaca <baca.zach@icloud.com>
2019-11-18 13:23:01 -0600

    created structure of login component

---
+++ replate/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login To Your Replate Account</h1>
+            <form>
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username" />
+                <br />
+                <label for="password">Password:</label>
+                <input type="text" name="password" id="password" />
+            </form>
+        </div>
+    )
+}
+
+export default Login;
