5b79c551422b4f8dc6349a9fcb751426cdad7935
zacharybaca <baca.zach@icloud.com>
2019-11-19 15:58:44 -0600

    created initial state for store and wrapped store around app

--- replate/src/index.js
+++ replate/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 
+import rootReducer from './store/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+    
+);
 
 
