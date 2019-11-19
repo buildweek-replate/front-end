049a1ce411ded080bb5eeeee045ea4fdc336fa9e
lukasjaronis <jaronisluke13@gmail.com>
2019-11-19 17:01:28 -0500

    able to create a new entry

--- replate/src/App.js
+++ replate/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.css';
 
 import BusinessSignUp from "./components/business/signup/BusinessSignUp";
+import BusinessDashboard from "./components/business/dashboard/BusinessDashboard";
+import BusinessPickUp from "./components/business/dashboard/BusinessPickup";
+
+
 
 
 function App() {
+
+
+
   return (
     <div className="App">
 
 
-    <BusinessSignUp />
+    <BusinessPickUp />
 
 
     </div>
