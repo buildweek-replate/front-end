423105ac3adad48591e34db21d56a7c044281b36
zacharybaca <baca.zach@icloud.com>
2019-11-19 18:22:45 -0600

    changes

--- replate/src/index.js
+++ replate/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
 
 import rootReducer from './store/reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
